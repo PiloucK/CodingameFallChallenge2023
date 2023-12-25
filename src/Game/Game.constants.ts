022a6632c999823d237b4f078b1628868011f843
PiloucK <clkuznie@student.42.fr>
2023-12-25 20:54:53 +0100

    add range and hitbox constants

--- src/Game/Game.constants.ts
+++ src/Game/Game.constants.ts
@@ -1,2 +1,7 @@
-const fullZoneViewWidthWithLight = 1560;
 export const MAP_SIZE = 10000;
+export const DRONE_MAX_MOVE_DIST = 600;
+export const DRONE_RADIUS = 200;
+export const MONSTER_RADIUS = 300;
+export const MONSTER_DANGER_PRECISION = 40;
+export const SAFETY_EPSYLON = 5;
+export const STATIC_SAFETY_RADIUS = DRONE_RADIUS + MONSTER_RADIUS + SAFETY_EPSYLON;
