fdaa0ea669ae8361ad386b864f7597b3116aa03c
PiloucK <clkuznie@student.42.fr>
2024-01-03 15:29:05 +0100

    remove multiline imports

--- fuseFiles.py
+++ fuseFiles.py
@@ -5,6 +5,7 @@ def aggregate_typescript_files(source_dir, output_file, ignore_dirs=None):
         ignore_dirs = []
 
     all_files_content = ""
+    import_seen = False  # Used to track multiline imports
 
     for root, dirs, files in os.walk(source_dir):
         # Skip ignored directories
@@ -16,10 +17,19 @@ def aggregate_typescript_files(source_dir, output_file, ignore_dirs=None):
                 with open(os.path.join(root, file), 'r') as f:
                     lines = f.readlines()
                 
-                # Filter out import statements
-                lines = [line for line in lines if not line.strip().startswith('import')]
-                all_files_content += ''.join(lines)
+                for line in lines:
+                    line_strip = line.strip()
+                    if line_strip.startswith('import'):
+                        if not line_strip.endswith(';'):
+                            import_seen = True  # Set flag for multiline import
+                        continue  # Skip adding this line regardless of its type
 
+                    if import_seen and line_strip.endswith(';'):
+                        import_seen = False  # Reset the flag at the end of a multiline import
+                        continue  # Also skip adding the ending line of a multiline import
+
+                    if not import_seen:
+                        all_files_content += line  # Add line if it's not part of an import statement
 
     all_files_content += "\nconst game = new Game();while (true) {game.playTurn();}\n"
 
